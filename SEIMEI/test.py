0298975bc2d6f13c43b35b2bcedc5f818b4457fd
kentarrito <sekikentaro43@gmail.com>
2024-11-19 06:27:54 +0900

    Update test.py

--- SEIMEI/test.py
+++ SEIMEI/test.py
@@ -6,3 +6,6 @@ expert_class_names = ["Answer", "CheckInf", "MetaSurvey"] # "StructureAnalysis",
 se_restrictions = ["MetaSurvey"]  # search engine only hits classes in this list usually (except when adding expert_restriction in kwargs)
 expert_module_names = ["Experts.Code.Modify"]
 
+test_text = "Hello, my name is SEIMEI!"
+num_token = SEIMEI.get_num_tokens(test_text)
+print(num_token)
