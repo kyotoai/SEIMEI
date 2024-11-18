074d3ffba05cbf5c116de5115ed865d07cbcd262
kentarrito <sekikentaro43@gmail.com>
2024-11-18 20:28:21 +0900

    Create test.py

---
+++ SEIMEI/test.py
@@ -0,0 +1,16 @@
+from SEIMEI import SEIMEI
+import asyncio
+
+processed_path = "./processed"  # input path same as save_path you used in Preparation
+expert_class_names = ["Answer", "CheckInf", "MetaSurvey"] # "StructureAnalysis", "ChunkSurvey", "FileSurvey", "MetaSurvey"]
+se_restrictions = ["MetaSurvey"]  # search engine only hits classes in this list usually (except when adding expert_restriction in kwargs)
+expert_module_names = ["Experts.Code.Modify"]
+
+seimei = SEIMEI(
+    processed_path = processed_path,
+    expert_class_names = expert_class_names,
+    expert_module_names = expert_module_names,
+    se_restrictions = se_restrictions,
+    max_inference_time = 300,
+    tensor_parallel_size = 1,
+)
