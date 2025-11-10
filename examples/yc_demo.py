f0fcb792ec7615a28b53526de21ed61f43805fc9
kentarrito <sekikentaro43@gmail.com>
2025-11-10 23:17:47 +0900

    Wrong Debug

--- examples/yc_demo.py
+++ examples/yc_demo.py
@@ -5,7 +5,7 @@ async def demo_code_act():
     orchestrator = seimei(
         #agent_config=[{"file_path": "seimei/agents/code_act.py"}],
         llm_kwargs={"model": "gpt-5-nano"}, #"base_url": "https://0lhuputae2fexu-8000.proxy.runpod.net/generate"},
-        rm_kwargs={"url": "https://phpb5lzs0u9reb-8000.proxy.runpod.net/rmsearch", "agent_routing":False, "knowledge_search":True},
+        rm_kwargs={"url": "https://gvfbtfaf09djo1-8000.proxy.runpod.net/rmsearch", "agent_routing":False, "knowledge_search":True},
         allow_code_exec=True,
         agent_log_head_lines=1,
         max_tokens_per_question=30000,
